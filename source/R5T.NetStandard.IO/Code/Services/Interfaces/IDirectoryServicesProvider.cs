c4be16940aa82a4412d69bf63bf2c80e0f6113f5
MinexAutomation <minex.automation@gmail.com>
2019-06-20 14:27:21 -0400

    Added file- and directory-services provider interfaces and implementations.

---
+++ source/R5T.NetStandard.IO/Code/Services/Interfaces/IDirectoryServicesProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.NetStandard.IO.Paths;
+
+
+namespace R5T.NetStandard.IO
+{
+    public interface IDirectoryServicesProvider
+    {
+        void CreateDirectory(DirectoryPath directoryPath);
+        bool DirectoryExists(DirectoryPath directoryPath);
+    }
+}
